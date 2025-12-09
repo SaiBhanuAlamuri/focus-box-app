9702b4539728a726b5b4ca7e7f0368ccf32fef64
SaiBhanuAlamuri <22a91a05k0@aec.edu.in>
2025-12-09 11:40:57 +0530

    Intial Template is Done

---
+++ src/Components/Learn.js
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+export default function Learn() {
+  return (
+    <div>
+      <h1>Started Journey  -Rock it Buddy</h1>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+    </div>
+  );
+}
---
+++ src/Template/Base.js
@@ -0,0 +1,26 @@
+import { Box } from "@mui/material";
+
+export default  function FullScreen()
+{
+
+    return(
+        <>
+
+
+
+        <Box
+            sx={{
+                width: '100vw',
+                height: '100vh',
+                background: 'linear-gradient(135deg, #1a2332 0%, #0f1419 100%)',
+                display: 'flex',
+                justifyContent: 'center',}}>
+           
+        </Box>
+
+
+
+        </>
+    )
+
+}
